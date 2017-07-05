c1acf001719c7397e8fa7fa2f096dce2c8a90722
jiangsssw <1941886426@qq.com>
2017-07-05 17:00:12 +0800

    jingdong

---
+++ jingdongshopping/js/jingdong.js
@@ -0,0 +1,21 @@
+
+function closed() {
+    var closed = document.getElementById("C-ad");
+    closed.style.display="none";
+}
+function yingchang() {
+    var a = document.getElementById("neirong1");
+    a.style.display="none";
+    var b = document.getElementById("neirong2");
+    b.style.display="block";
+}
+function zhanshi() {
+    var a = document.getElementById("neirong1");
+    a.style.display="block";
+    var b = document.getElementById("neirong2");
+    b.style.display="none";
+}
+
+/**
+ * Created by 19418 on 2017/5/25.
+ */
